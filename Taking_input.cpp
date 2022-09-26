4456ff3c7a78c1ff48b3d47a832a55c57ac5a749
Thakor Jenil Kamleshbhai <103255558+Jenil45@users.noreply.github.com>
2022-09-26 18:24:26 +0530

    Create Taking_input.cpp

---
+++ Taking_input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>                           // We will find the sum of two number here
+using namespace std;
+
+int main()
+{
+    int amount1;
+    cin>>amount1;                             // cin use to take input from user
+
+    int amount2;
+    cin>>amount2;
+
+    int sum=amount1+amount2;
+    cout<<"sum is "<<sum;
+
+    return 0;
+}
