ffec58f3494454d9acb761c042cdc3b7ff9786c3
Thakor Jenil Kamleshbhai <103255558+Jenil45@users.noreply.github.com>
2022-10-01 17:08:56 +0530

    Create full_pyramid.cpp

---
+++ Star Pattern/full_pyramid.cpp
@@ -0,0 +1,40 @@
+// ADVANCE PATTERN QUESTION 4
+/*
+4) NUMBER PATTERN for n=5
+
+     1                                     ROWS : 1 to n 
+    1 2                                    SPACE : 5+1-i
+   1 2 3                                   j = 1 to row no.
+  1 2 3 4
+ 1 2 3 4 5
+
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n-i; j++)
+        {
+            cout<<" ";
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<j<<" ";
+        }
+        
+         for (int j = 1; j <= n-i; j++)
+        {
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
