b15053c86df9a95c48f5ebc6ba86eddeb6c93f64
Thakor Jenil Kamleshbhai <103255558+Jenil45@users.noreply.github.com>
2022-10-07 22:22:22 +0530

    Create match_pair_sum_O(n).cpp

---
+++ match_pair_sum_O(n).cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+using namespace std;
+//Check which pairsum is match with k (time complexity = o(n))
+
+bool pairsum(int arr[] , int n , int k)
+{
+    int low = 0;
+    int high = n-1;
+
+    while (low<high)
+    {
+        if (arr[low]+arr[high] == k)
+        {
+            cout<<low<<" "<<high<<endl;
+            return true;
+        }
+
+        else if (arr[low]+arr[high] > k)
+        {
+            high--;
+        }
+
+        else
+        {
+            low++;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int n, k ;
+    cin>>n>>k;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    cout<<pairsum(arr , n , k)<<endl;
+    return 0;
+}
