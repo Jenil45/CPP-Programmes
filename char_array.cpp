aa31184ea0946f8f3a46ab74415251d7d42eeaf6
Thakor Jenil Kamleshbhai <103255558+Jenil45@users.noreply.github.com>
2022-10-09 16:10:01 +0530

    Create char_array.cpp

---
+++ char_array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char arr[100];
+    cin>>arr;                                       //Taking name input
+    cout<<arr<<"\n";                                      //Printing output
+
+    cout<<arr[2];                                   //Print specific character  
+    return 0;
+}
